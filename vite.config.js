06514078ada88a5b7663cb83f19bdfa6720b9aba
Emmanuel <compuioelijo@gmail.com>
2025-11-05 00:45:49 -0300

    fix: added vercel rewrite and vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 asegura rutas relativas correctas al construir
 })
